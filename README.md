41b1d680c0640e58ed098fd04b4c70ca998bf225
Dax Buratto <daxburatto@gmail.com>
2021-03-15 18:26:10 -0500

    added delete and post router functions, and added some readme

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Notetakr
+# NOTETAKr
+
+By daxburatto
+
+## Description
+
+Notetakr is a simple app for people who need a clean and quick note taking app, you can enter a title and body of your note and save and delete them at your needs
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+
+Find it at the link provided
+
+## Usage
+
+Organization
+
+## Credits
+
+Email: daxburatto@gmail.com
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,8 +2,23 @@ const router = require('express').Router()
 const db = require('../../db/db.json')
 const { saveNote, findById, deleteNote } = require('../../lib/notes')
 
-router.get('/notes', (req, res) => {
-    res.json(notes)
+router.get('/db', (req, res) => {
+    res.json(db)
+})
+
+router.delete('/db/:id', (req, res) => {
+    const result = findById(req.params.id, db)
+    if (result) {
+        deleteNote(req.params.id, db)
+        res.json(db)
+    } else {
+        res.send(404)
+    }
+})
+
+router.post('/db', (req, res) => {
+    const note = saveNote(req.body, db)
+    res.json(note)
 })
 
 module.exports = router
