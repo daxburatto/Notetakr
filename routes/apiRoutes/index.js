6eaa7cc87b1ecf7446b52b88f478c27f80471417
Dax Buratto <daxburatto@gmail.com>
2021-03-15 16:19:47 -0500

    added notes.js to save, find, and delete notes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,9 @@
 const router = require('express').Router()
 const db = require('../../db/db.json')
+const { saveNote, findById, deleteNote } = require('../../lib/notes')
 
+router.get('/notes', (req, res) => {
+    res.json(notes)
+})
+
+module.exports = router
